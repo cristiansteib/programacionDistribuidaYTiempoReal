a6fc9c848d23212f57a90cfc248fc705b199a55e
Cristian Steib <cristiansteib@gmail.com>
2019-11-21 02:20:07 -0300

    Get anda bien :)

--- practica4/Ej3/FileSystem.java
+++ practica4/Ej3/FileSystem.java
@@ -1,99 +1,95 @@
 package Ej3;
+
 import jade.core.*;
 
-import java.lang.IndexOutOfBoundsException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Serializable;
 
 //-container -host localhost AgenteCris:Ej3.FileSystem(send,testFileX)
+
 public class FileSystem extends Agent {
     private Location origin;
-    private String fileName;
-    private String mode;
-
-    private FileData file;
-    private int offset = 0;
+    private static final int  CHUNK_SIZE = 20;
 
     public void setup() {
+
+        this.origin = here();
+        String operation = (String) getArguments()[0];
+        String fileName = (String) getArguments()[1];
+
+        switch (operation) {
+            case "get":
+                try {
+                    getFile(fileName);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.exit(2);
+                }
+                break;
+            case "send":
+                try {
+                    sendFile(fileName);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.exit(2);
+                }
+                break;
+            default:
+                System.out.println("Parameter error, user get|send operations.");
+        }
+    }
+
+
+    private void getFile(String fileName) throws IOException {
+        int offset = 0;
+        Location serverLocation = new ContainerID("Main-Container", null);
+        String localFilePath =  "Ej3/fs_local/" + fileName;
+        String serverFilePath = "Ej3/fs_server/" + fileName;
+        // Lo muevo al server
+
+        while (true) {
+            doMove(serverLocation);
+            FileData f = read(serverFilePath, offset, CHUNK_SIZE); // Lee el archivo del server
+            offset += CHUNK_SIZE;
+
+            doMove(this.origin);
+            write(localFilePath, f); // escribe en el origen
+            if (f.finished()) {
+                break;
+            }
+        }
+    }
+
+    private void sendFile(String fileName) throws IOException {
+        int offset = 0;
+        int chunkSize = 20;
+        Location destination = new ContainerID("Main-Container", null);
+        String originFilePath = "Ej3/fs_local/" + fileName;
+        String serverFilePath = "Ej3/fs_server/" + fileName;
+
+        FileData f = read(originFilePath, offset, chunkSize);  // Leo el archivo del origen
+        offset += offset + chunkSize;
+        doMove(destination);  // Lo muevo al server
+        write(serverFilePath, f); // escribe en el server
+        while (!f.finished()) {
+            doMove(this.origin);
+            f = read(originFilePath, offset, chunkSize); // lee en el origen el file
+            offset += offset + chunkSize;
+            doMove(destination); // Vuelve al server
+            write(serverFilePath, f); // escribe en el server el file leido
+        }
+    }
+
+
+    public int write(String filePath, FileData fileData) {
         try {
-            this.origin = here();
-            this.mode = (String)getArguments()[0];
-            this.fileName = (String)getArguments()[1];
-
-            if(this.mode.equals("get") && this.mode.equals("send")) {
-                throw new IllegalArgumentException();
-            }
-
-            System.out.println("Se traera el archivo " + this.fileName);
-            doMove(new ContainerID("Main-Container", null));
-        } catch(IllegalArgumentException e) {
-            System.out.println("Parameter error, user read|write operations.");
-        } catch (Exception e) {
-            System.out.println("Error al miragrar el agente");
-            e.printStackTrace();
-        }
-    }
-
-    protected void readFile(Location next){
-        System.out.println("\nLeo\n");
-
-        if(this.file == null || !this.file.finished()) {
-            try {
-                this.file = read(this.fileName, this.offset, 1024);
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.exit(2);
-            }
-            doMove(next);
-        } else {
-            System.out.println("\nSe termino de transferir el archivo " + this.fileName + "\n");
-        }
-    }
-
-    protected void afterMove() {
-        Location next = atHome() ? new ContainerID("Main-Container", null) : this.origin;
-
-        System.out.println("\nqeu onda\n");
-
-        if((atHome() && this.mode.equals("get")) || (!atHome() && this.mode.equals("send"))) {
-            if (this.file == null){
-                System.out.println("\nEs null, muevo\n");
-                readFile(next);
-
-            }
-            System.out.println(this.file);
-
-            write(this.file);
-            this.offset += this.file.getAmount();
-            if (!this.file.finished()) {
-
-                doMove(next);
-            } else {
-                System.out.println("\nSe termino de transferir el archivo " + this.fileName + "\n");
-            }
-        } else {
-            readFile(next);
-
-        }
-    }
-
-    public boolean atHome() {
-        return here().getID().equals(this.origin.getID());
-    }
-
-    public int write(FileData fileData) {
-        try {
-            File file = new File("Ej3/local/" + fileData.getName());
-            System.out.println("Escribiendo: "+fileData.getName());
-
-            FileOutputStream fos = new FileOutputStream(file,true);
-            fos.write(fileData.getData(),0,fileData.getAmount());
+            File file = new File(filePath);
+            System.out.println("Escribiendo: " + filePath);
+            FileOutputStream fos = new FileOutputStream(file, true);
+            fos.write(fileData.getData(), 0, fileData.getAmount());
             fos.close();
             System.out.println("Se escribieron " + fileData.getAmount() + " bytes en " + fileData.getName());
             return fileData.getAmount();
@@ -103,22 +99,30 @@ public class FileSystem extends Agent {
         }
     }
 
-    public FileData read(String fileName, int offset, int amount) throws IOException {
+    public FileData read(String filePath, int offset, int amount) throws IOException {
         try {
-            File file = new File("Ej3/SRC_SERVER/" + fileName);
-            System.out.println("LEyendo: "+fileName);
+            System.out.println(filePath);
+            File file = new File(filePath);
+
             FileInputStream fis = new FileInputStream(file);
             fis.skip(offset);
-            byte[] bytesArray = new byte[Math.min(amount,fis.available())];
+
+            byte[] bytesArray = new byte[Math.min(amount, fis.available())];
             int amountRead = fis.read(bytesArray);
+
             boolean finished = fis.available() == 0;
+            System.out.println(fis.available());
             fis.close();
-            System.out.println("Se leyeron " + amountRead + " bytes de " + fileName);
-            return new FileData(fileName,bytesArray,finished,amountRead);
+
+            System.out.println("Se leyeron " + amountRead + " bytes de " + filePath);
+            System.out.println(finished);
+
+            return new FileData(filePath, bytesArray, finished, amountRead);
+
         } catch (IOException e) {
-            System.out.println("Error leyendo archivo " + fileName);
+            System.out.println("Error leyendo archivo " + filePath);
             e.printStackTrace();
-            throw  new IOException();
+            throw new IOException();
         }
     }
 }
